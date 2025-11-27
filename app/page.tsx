aa227df067c5a02ff295f645472c2b86e21ca1c5
ancika37 <ancikaregmi77@gmail.com>
2025-11-27 20:23:16 +0545

    second change

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { Happy_Monkey } from "next/font/google";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
       <h1>Ancika Regmi</h1>
+      rhishav monkey
     </div>
   );
 }
